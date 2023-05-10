fbad9b07ad5a7d926e516a5d4ea19c06c724d463
susatthi <susa@keyber.jp>
2023-05-10 09:13:36 +0900

    🚀 feat: 基本実装

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'page/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Material Color Tonal Palettes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
