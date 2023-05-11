e899c8c6e66eaaab7f644355d958f22ffcf74d88
susatthi <susa@keyber.jp>
2023-05-11 16:20:36 +0900

    🚀 feat: #除去

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'app.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(
     const ProviderScope(
       child: App(),
